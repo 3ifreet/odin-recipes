5c6d9b32847a40c54df12bcfbe0bce0857b2ad1a
3ifreet <mahdikhalidkhalil@gmail.com>
2023-02-03 17:21:51 +0300

    added set grid size feature

--- ItchASketch/script.js
+++
@@ -1,6 +0,0 @@
-const myNodeList = document.querySelectorAll(".grid-item");
-myNodeList.forEach((item)=>{
-    item.addEventListener("hover", ()=>{
-                item.style.backgroundColor = "000000";
-    })
-})
